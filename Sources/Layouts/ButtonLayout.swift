05be9c9869e36da43c02e052748ebdde71221195
Kyrylo Khlopko <khlopko@gmail.com>
2020-09-01 17:26:03 +0300

    Add button layout sizing options to layout with the container or custom height.

--- Sources/Layouts/ButtonLayout.swift
+++ Sources/Layouts/ButtonLayout.swift
@@ -27,11 +27,23 @@ public struct ButtonElementInsets {
 /// Layout for the button element.
 public struct ButtonLayout : Layout {
 
+    /// Size calculation options.
+    public enum SizeOption {
+
+        /// Layout will use height of the parent rect.
+        case parentHeight
+
+        /// Layout will use custom provided height.
+        case customHeight(CGFloat)
+
+    }
+
     public typealias Child = Layout & ButtonElement
 
     private var child: Child
     private let imageSize: CGSize?
     private let insets: Insets
+    private let option: SizeOption?
     private let alignment: Alignment
 
     /// Initializer for the button with the square image.
@@ -44,17 +56,25 @@ public struct ButtonLayout : Layout {
         self.init(child: child, imageSize: CGSize(width: imageSide, height: imageSide), alignment: alignment)
     }
 
-    /// Initializer for the button with the given image size and insets.
+    /// Initializer for the button with the given image size, insets and sizing option.
     ///
     /// - Parameters:
     ///     - child: Element to be layouted.
     ///     - imageSize: Size of the element. Can be `nil`. Default is `nil`.
     ///     - insets: Insets for the button around image. Default is `.zero`.
+    ///     - option: Sizing option for the button, used in size calculation.
     ///     - alignment: Alignment of the element in the parent. By default it is top left corner for the origin.
-    public init(child: Child, imageSize: CGSize? = nil, insets: Insets = .zero, alignment: Alignment = []) {
+    public init(
+        child: Child,
+        imageSize: CGSize? = nil,
+        insets: Insets = .zero,
+        option: SizeOption? = nil,
+        alignment: Alignment = []
+    ) {
         self.child = child
         self.imageSize = imageSize
         self.insets = insets
+        self.option = option
         self.alignment = alignment
     }
 
@@ -71,25 +91,51 @@ public struct ButtonLayout : Layout {
         if let storage = child.textContent {
             titleSize = storage.size(constrainedTo: rect.size, useDeviceMetrics: false)
         }
-        var imageSize: CGSize = .zero
-        if let specifiedImageSize = self.imageSize {
-            imageSize = specifiedImageSize
-        } else if let storage = child.nestedImageElement?.imageContent {
-            imageSize = storage.size
-        }
+        let imageSize = finalImageSize
         let contentInsets = child.elementContentInsets
-        let width =
-            insets.left +
-            contentInsets.image.left + imageSize.width + contentInsets.image.right +
-            contentInsets.title.left + titleSize.width + contentInsets.title.right +
-            insets.right
-        let height =
-            insets.top +
-            max(contentInsets.image.top, contentInsets.title.top) +
-            max(imageSize.height, titleSize.height) +
-            max(contentInsets.image.bottom, contentInsets.title.bottom) +
-            insets.bottom
+        let width = calculateWidth(in: rect, contentInsets: contentInsets, imageSize: imageSize, titleSize: titleSize)
+        let height = calculateHeight(in: rect, contentInsets: contentInsets, imageSize: imageSize, titleSize: titleSize)
         return CGSize(width: width, height: height)
+    }
+
+    private var finalImageSize: CGSize {
+        if let specifiedImageSize = imageSize {
+            return specifiedImageSize
+        } else if let storage = child.nestedImageElement?.imageContent {
+            return storage.size
+        }
+        return .zero
+    }
+
+    private func calculateWidth(
+        in rect: CGRect,
+        contentInsets: ButtonElementInsets,
+        imageSize: CGSize,
+        titleSize: CGSize
+    ) -> CGFloat {
+        insets.left
+        + contentInsets.image.left + imageSize.width + contentInsets.image.right
+        + contentInsets.title.left + titleSize.width + contentInsets.title.right
+        + insets.right
+    }
+
+    private func calculateHeight(
+        in rect: CGRect,
+        contentInsets: ButtonElementInsets,
+        imageSize: CGSize,
+        titleSize: CGSize
+    ) -> CGFloat {
+        if let option = option {
+            switch option {
+            case .parentHeight: return rect.height
+            case let .customHeight(height): return height
+            }
+        }
+        return insets.top
+            + max(contentInsets.image.top, contentInsets.title.top)
+            + max(imageSize.height, titleSize.height)
+            + max(contentInsets.image.bottom, contentInsets.title.bottom)
+            + insets.bottom
     }
 
 }
