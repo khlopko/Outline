db58b0c28d0268549794bf3833ba9e4ead98f2b5
Kyrylo Khlopko <khlopko@gmail.com>
2020-11-05 14:48:14 +0200

    Add factory methods producing `InsetLayout` to all `Layout` types.

--- Sources/Layouts/InsetsLayout.swift
+++ Sources/Layouts/InsetsLayout.swift
@@ -29,3 +29,11 @@ public struct InsetLayout : Layout {
     }
 
 }
+
+extension Layout {
+
+    func inset(by insets: Insets) -> Layout { InsetLayout(child: self, insets: insets) }
+
+    func insetBy(dx: CGFloat, dy: CGFloat) -> Layout { InsetLayout(child: self, dx: dx, dy: dy) }
+
+}
